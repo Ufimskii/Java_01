ba23e92ed2495e1fd196d03039b423491e53c750
Ufimskii <alexander1383@list.ru>
2021-10-28 19:32:04 +0500

    java01_02

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int account = 2_000_000_000;
         int transfer = 500_000_000;
-        int totacc = account + transfer;
-        System.out.println(totacc);
+        int totalAccount = account + transfer;
+        System.out.println(totalAccount);
     }
 }
